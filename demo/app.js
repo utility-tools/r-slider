cba1d920d925cc6cbc18af37923a74ec0d77b851
kumar.ashish <kumar.ashish@servicenow.com>
2022-07-31 17:27:58 +0530

    removed comments

--- demo/app.js
+++ demo/app.js
@@ -2,13 +2,11 @@
     const slider = new RSlider({
         selector: document.querySelector('#r-slider'),
         gap: 20, // in px
-        // visibleItems: 3, // in number
         visibleItems: {
             large: 3,
             medium: 1,
             small: 1
         },
-        // edgeItemWidth: 100, // in px
         edgeItemWidth: {
             large: 100,
             medium: 50,
